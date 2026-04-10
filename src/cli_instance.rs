c51def685c0998eca117998f16489191b7eb284d
RayX <xxfanraina@gmail.com>
2026-04-10 17:04:25 +0800

    Update dragonball

--- src/cli_instance.rs
+++ src/cli_instance.rs
@@ -56,7 +56,7 @@ impl CliInstance {
         let vmm_shared_info = Arc::new(RwLock::new(InstanceInfo::new(
             String::from(id),
             DRAGONBALL_VERSION.to_string(),
-            ConfidentialVmType::None,
+            ConfidentialVmType::TDX,
         )));
 
         let to_vmm_fd = EventFd::new(libc::EFD_NONBLOCK)
