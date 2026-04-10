fa549906e84aab4e0a5d1ec8aa54d2b6a0b61918
RayX <xxfanraina@gmail.com>
2026-04-10 15:34:02 +0800

    Update dragonball

--- src/cli_instance.rs
+++ src/cli_instance.rs
@@ -130,6 +130,7 @@ impl CliInstance {
             path_on_host: PathBuf::from(&args.rootfs_args.rootfs.unwrap()),
             is_root_device: args.rootfs_args.is_root,
             is_read_only: args.rootfs_args.is_read_only,
+            use_generic_irq: Some(false),
             ..block_device_config_info
         };
 
