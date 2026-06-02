c4cb509d1c2a8a8eec9836484aea3ab4854f4346
Xiaofan Xxf <xiaofan.xxf@antgroup.com>
2026-06-02 13:59:48 +0800

    Fix tdshim path

--- src/cli_instance.rs
+++ src/cli_instance.rs
@@ -120,7 +120,7 @@ impl CliInstance {
             kernel_path: args.kernel_path.unwrap(),
             initrd_path: args.initrd_path.clone(),
             boot_args: Some(args.boot_args.clone()),
-            firmware_path: Some(String::from("/home/xiaofan/src/test_resources/tdshim.bin")),
+            firmware_path: Some(String::from("/home/xiaofan/src/test_resources/tdshim_largepayload.bin")),
         };
 
         // rootfs
